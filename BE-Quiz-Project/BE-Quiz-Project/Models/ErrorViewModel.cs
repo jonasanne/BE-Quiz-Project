bea24cb85e86334bd5d5a4854db91a2e2499a3e9
jonasanne <jonasanne@outlook.com>
2020-03-11 13:03:01 +0100

    init

---
+++ BE-Quiz-Project/BE-Quiz-Project/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace BE_Quiz_Project.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
